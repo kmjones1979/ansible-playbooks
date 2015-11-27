015de34eef2567dc757606d039bcafffbfb5cf46
vagrant <vagrant@localhost.localdomain>
2015-11-27 14:49:14 -0500

    adding setup script for non-ansible install

---
+++ bin/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum clean all && \
+    sudo yum -y install epel-release && \
+    sudo yum -y install PyYAML python-jinja2 python-httplib2 python-keyczar python-paramiko python-setuptools git python-pip
+
+sudo pip install ansible
