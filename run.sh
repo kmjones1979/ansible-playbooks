5d467e0be51d6e333fbe33771eb83dce0c64612e
Kevin Jones <jones.kevin@gmail.com>
2015-11-26 21:04:25 -0800

    minor changes

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 docker run -i -t --rm \
   --name docker-ansible docker-ansible \
-  ansible-playbook /$1 -s -k -u vagrant --ask-vault-pass
+  /bin/bash
+  #ansible-playbook /$2 -i /$1 -s -k -u vagrant --ask-vault-pass
