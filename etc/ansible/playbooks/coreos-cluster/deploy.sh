d5bcbd1f481b46817411550ae1a60bec4223f534
Kevin Jones <jones.kevin@gmail.com>
2015-11-27 17:20:07 -0500

    changes

--- etc/ansible/playbooks/coreos-cluster/deploy.sh
+++ etc/ansible/playbooks/coreos-cluster/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# ansible deployment
+
 #ansible-playbook -i inventory/hosts bootstrap.yml
 ansible-playbook -vvvv -i inventory/hosts deploy.yml --ask-vault-pass
+
+#if [ -a /tmp/insecure_private.key ]
+#then
+#  shred -uvf -n 10 /tmp/insecure_private.key
+#fi
