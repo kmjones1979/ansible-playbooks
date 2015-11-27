c614577bbcde5f7865d63c2a72d71cf5ef38fe0e
Kevin Jones <jones.kevin@gmail.com>
2015-11-27 01:36:07 -0800

    adding changes

--- etc/ansible/playbooks/coreos-cluster/deploy.sh
+++ etc/ansible/playbooks/coreos-cluster/deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ -a /etc/coreos/bootstrap ]
-then
-  ansible-playbook deploy.yml -i inventory/hosts --ask-vault-pass
-else
-  ansible-playbook bootstrap.yml -i inventory/hosts
-fi
+ansible-playbook deploy.yml -i inventory/hosts --ask-vault-pass
 
